65db8204bf03209b6b375781af21b1b6c0cd06c9
Jason Noah Choi <jasonnoahchoi@gmail.com>
2020-09-02 22:22:07 -0400

    create exports and SecurityProvider for naming

---
+++ gatsby-plugin-okta/index.js
@@ -0,0 +1,7 @@
+import { useOktaAuth } from '@okta/okta-react'
+
+export { useOktaAuth }
+export { default as useGatsbyAuth } from './src/hooks/useGatsbyAuth'
+export { default as LoginCallback } from './src/components/callback'
+export { default as SecurityProvider } from './src/context/SecurityProvider'
+export { default as config } from './src/auth/config'
---
+++ gatsby-plugin-okta/src/context/SecurityProvider.js
@@ -0,0 +1,7 @@
+/**
+ * SecurityProvider is just `Security` with a more React-y name
+ *
+ */
+
+import { Security as SecurityProvider } from '@okta/okta-react'
+export default SecurityProvider
