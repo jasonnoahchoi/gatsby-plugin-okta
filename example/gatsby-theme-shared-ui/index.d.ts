d2e082ff1d91a53f555ecda94677797e587dade4
Jason Noah Choi <jasonnoahchoi@gmail.com>
2020-09-03 12:37:44 -0400

    feat: create and export NavLinks for example proj

--- example/gatsby-theme-shared-ui/index.d.ts
+++ example/gatsby-theme-shared-ui/index.d.ts
@@ -6,9 +6,10 @@ import {
   TableHead,
   TableHeader,
 } from './src/components/Table'
+import { Link, ExternalLink, NavLink } from './src/components/Link'
 export { default as Layout } from './src/components/layout'
 export { default as Form } from './src/components/Form'
 export { default as Header } from './src/components/Header'
 export { default as PrivateRoute } from './src/components/PrivateRoute'
-export { default as Link } from './src/components/Link'
+export { Link, ExternalLink, NavLink }
 export { Table, TableBody, TableCell, TableRow, TableHead, TableHeader }
--- example/gatsby-theme-shared-ui/src/components/Link.js
+++ example/gatsby-theme-shared-ui/src/components/Link.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link as ChakraLink, Icon } from '@chakra-ui/core'
+import { Link as ChakraLink, Icon, Box } from '@chakra-ui/core'
 import { Link as GatsbyLink } from 'gatsby'
 
 export function Link({ to, children }, props) {
@@ -15,5 +15,15 @@ export function ExternalLink({ href, children }) {
     <ChakraLink href={href} color="gray.500" isExternal>
       {children} <Icon name="external-link" mx="2px" paddingBottom="3px" />
     </ChakraLink>
+  )
+}
+
+export function NavLink({ to, children }, props) {
+  return (
+    <Box as="h3" paddingTop="10px">
+      <ChakraLink as={GatsbyLink} to={to} {...props}>
+        {children}
+      </ChakraLink>
+    </Box>
   )
 }
