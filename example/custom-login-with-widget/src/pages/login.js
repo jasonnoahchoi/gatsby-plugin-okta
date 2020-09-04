c7b51272dd94b5f15d528e2fc391c943811f164b
Jason Noah Choi <jasonnoahchoi@gmail.com>
2020-09-03 20:37:54 -0400

    bugfix: fix crash by removing widget on unmount
    - add gatsby logo to widget
    - use imported widget from plugin

--- example/custom-login-with-widget/src/pages/login.js
+++ example/custom-login-with-widget/src/pages/login.js
@@ -1,29 +1,43 @@
-import React, { useEffect } from 'react'
-
-import { config } from 'gatsby-plugin-okta'
-
-import OktaSignInWidget from '../components/OktaSignInWidget'
+import React, { useState, useEffect } from 'react'
+import { OktaSignInWidget, config } from 'gatsby-plugin-okta'
+import { Box } from '@chakra-ui/core'
 
 export default function Login() {
+  const [renderWidget, setRenderWidget] = useState(false)
+
+  const logo = 'https://www.gatsbyjs.com/Gatsby-Monogram.svg'
+  const widget = OktaSignInWidget({ config, logo })
   useEffect(() => {
-    const widget = OktaSignInWidget(config)
-    widget.renderEl(
-      { el: '#sign-in-widget' },
-      () => {
-        /**
-         * In this flow, the success handler will not be called beacuse we redirect
-         * to the Okta org for the authentication workflow.
-         */
-      },
-      (err) => {
-        throw err
+    if (!renderWidget) {
+      widget.renderEl(
+        { el: '#sign-in-widget' },
+        () => {
+          /**
+           * In this flow, the success handler will not be called beacuse we redirect
+           * to the Okta org for the authentication workflow.
+           * but otherwise you can do something like `success(res)` and handle it appropriately
+           */
+        },
+        (err) => {
+          throw err
+        }
+      )
+      setRenderWidget(true)
+    }
+  }, [renderWidget])
+
+  useEffect(() => {
+    return () => {
+      if (renderWidget) {
+        widget.remove()
+        setRenderWidget(false)
       }
-    )
-  }, [])
+    }
+  }, [renderWidget])
 
   return (
-    <div>
-      <div id="sign-in-widget" />
-    </div>
+    <Box>
+      <Box id="sign-in-widget" />
+    </Box>
   )
 }
