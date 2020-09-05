243718036816cffd042bbd44a6b7b46877a89661
Jason Noah Choi <jasonnoahchoi@gmail.com>
2020-09-05 14:56:07 -0400

    allow SSR for widget, turn it into a component

--- example/custom-login-with-widget/src/pages/login.js
+++ example/custom-login-with-widget/src/pages/login.js
@@ -1,47 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { OktaSignInWidget, config } from 'gatsby-plugin-okta'
+import React from 'react'
+import { config, OktaSignInWidget } from 'gatsby-plugin-okta'
 import { Box } from '@chakra-ui/core'
 
 const logo = 'https://www.gatsbyjs.com/Gatsby-Monogram.svg'
-const widget = OktaSignInWidget({
-  config,
-  logo,
-  title: 'Custom Login with Widget',
-})
 
 export default function Login() {
-  const [renderWidget, setRenderWidget] = useState(false)
-  useEffect(() => {
-    if (!renderWidget) {
-      widget.renderEl(
-        { el: '#sign-in-widget' },
-        () => {
-          /**
-           * In this flow, the success handler will not be called beacuse we redirect
-           * to the Okta org for the authentication workflow.
-           * but otherwise you can do something like `success(res)` and handle it appropriately
-           */
-        },
-        (err) => {
-          throw err
-        }
-      )
-      setRenderWidget(true)
-    }
-  }, [renderWidget])
-
-  useEffect(() => {
-    return () => {
-      if (renderWidget) {
-        widget.remove()
-        setRenderWidget(false)
-      }
-    }
-  }, [renderWidget])
-
   return (
     <Box>
-      <Box id="sign-in-widget" />
+      <OktaSignInWidget config={config} logo={logo} title="Login" />
     </Box>
   )
 }
--- gatsby-plugin-okta/src/components/OktaSignInWidget.js
+++ gatsby-plugin-okta/src/components/OktaSignInWidget.js
@@ -1,44 +1,74 @@
-import OktaSignIn from '@okta/okta-signin-widget'
+import React, { useEffect } from 'react'
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
 
 /**
  * OktaSignIn component
  *
- * The okta sign in widget requires that you are not using SSR at this time.
- * This is something that will be handled in a future PR hopefully.
+ *
  * It also requires that you set a config.
  *
  * @param {object} config - Required configuration object that can be found at `src/auth/config`
  * @param {string} title - Optional string that provides title for widget
  * @param {string} logo - Optional logo path
  *
- * @returns {object} widget
+ * @returns {object} <div />
  */
 
 export default function OktaSignInWidget({ config, title, logo }) {
-  const { pkce, issuer, clientId, redirectUri, scopes, domain } = config
-  const widget = new OktaSignIn({
-    /**
-     * Note: when using the Sign-In Widget for an OIDC flow, it still
-     * needs to be configured with the base URL for your Okta Org. Here
-     * we derive it from the given issuer for convenience.
-     */
-    baseUrl: domain || issuer.split('/oauth2')[0],
-    clientId,
-    redirectUri,
-    logo,
-    i18n: {
-      en: {
-        'primaryauth.title': title,
-      },
-    },
-    authParams: {
-      pkce,
-      issuer,
-      display: 'page',
-      responseMode: pkce ? 'query' : 'fragment',
-      scopes,
-    },
-  })
-  return widget
+  useEffect(() => {
+    let widget
+    const { pkce, issuer, clientId, redirectUri, scopes, domain } = config
+    // https://github.com/gatsbyjs/gatsby/issues/309
+    import('@okta/okta-signin-widget')
+      .then((oktaSignIn) => {
+        const OktaSignIn = oktaSignIn.default
+        function createWidget() {
+          widget = new OktaSignIn({
+            /**
+             * Note: when using the Sign-In Widget for an OIDC flow, it still
+             * needs to be configured with the base URL for your Okta Org. Here
+             * we derive it from the given issuer for convenience.
+             */
+            baseUrl: domain || issuer.split('/oauth2')[0],
+            clientId,
+            redirectUri,
+            logo,
+            i18n: {
+              en: {
+                'primaryauth.title': title,
+              },
+            },
+            authParams: {
+              pkce,
+              issuer,
+              display: 'page',
+              responseMode: pkce ? 'query' : 'fragment',
+              scopes,
+            },
+          })
+          widget.renderEl(
+            { el: '#sign-in-widget' },
+            () => {
+              /**
+               * In this flow, the success handler will not be called beacuse we redirect
+               * to the Okta org for the authentication workflow.
+               * but otherwise you can do something like `success(res)` and handle it appropriately
+               */
+            },
+            (err) => {
+              throw err
+            }
+          )
+        }
+        createWidget()
+      })
+      .catch((e) => console.warn(e))
+    return () => {
+      if (widget) {
+        widget.remove()
+      }
+    }
+  }, [config, title, logo])
+
+  return <div id="sign-in-widget" />
 }
