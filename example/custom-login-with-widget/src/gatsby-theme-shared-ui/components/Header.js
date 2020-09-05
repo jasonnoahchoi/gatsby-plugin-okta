2b52d1bac07a4b478648be102e7328a8495eac7d
Jason Noah Choi <jasonnoahchoi@gmail.com>
2020-09-05 14:55:24 -0400

    allow usage of chakra-ui in all of custom-widget

--- example/custom-login-with-widget/src/gatsby-theme-shared-ui/components/Header.js
+++ example/custom-login-with-widget/src/gatsby-theme-shared-ui/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useOktaAuth } from 'gatsby-plugin-okta'
-import { Button, Box, Text } from '@chakra-ui/core'
+import { Button, Box, Text, useColorMode } from '@chakra-ui/core'
 import { NavLink } from 'gatsby-theme-shared-ui'
 import { navigate } from 'gatsby'
 import siteConfig from '../../../site-config'
 
 const headerStyle = {
   display: 'flex',
-  backgroundColor: '#fff',
   borderBottom: '1px solid #e7e7e7',
   justifyContent: 'space-between',
   padding: 10,
@@ -15,6 +14,7 @@ const headerStyle = {
 
 export default function Header() {
   const { authState, authService } = useOktaAuth()
+  const { colorMode, toggleColorMode } = useColorMode()
 
   const login = async () => {
     navigate('/login')
@@ -25,13 +25,16 @@ export default function Header() {
 
   return (
     <header>
-      <Box style={headerStyle}>
+      <Box as="nav" style={headerStyle}>
         <Text fontWeight="bold" paddingTop="10px">
           {siteConfig.siteTitle}
         </Text>
         <NavLink to="/home">Home</NavLink>
         <NavLink to="/landing">Landing</NavLink>
         <NavLink to="/profile">Profile</NavLink>
+        <Button onClick={toggleColorMode}>
+          {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
         <Button onClick={authState.isAuthenticated ? logout : login}>
           {authState.isAuthenticated ? 'Logout' : 'Login'}
         </Button>
